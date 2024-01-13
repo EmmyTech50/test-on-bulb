00c60039250dd7c238f9f439755fb4bb18b82468
EmmyTech50 <emmagospelnews@gmail.com>
2024-01-13 00:30:18 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+function light(show){
+    let bulb;
+    
+    if(show == 0){
+        bulb = 'BulbOFF.JPG';
+    }else{
+        bulb = 'BulbON.JPG';
+    }
+    document.getElementById("lightbulb").src = bulb;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
